ff1f2bb56de7d8b311619f6e5c9e413686d15114
Seth Bonnie <sethkbonnie@gmail.com>
2017-06-18 09:14:28 -0500

    Use actual hrefs instead of a hash to remove warnings

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -5,22 +5,22 @@ import './footer.css';
 const Footer = ({show}) => (
   <div id="footer" className={classnames({hide: !show})}>
     <ul>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Help Center</a></li>
-      <li><a href="#">Blog</a></li>
-      <li><a href="#">Status</a></li>
-      <li><a href="#">Jobs</a></li>
-      <li><a href="#">Terms</a></li>
-      <li><a href="#">Privacy Policy</a></li>
-      <li><a href="#">Cookies</a></li>
-      <li><a href="#">Ads Info</a></li>
-      <li><a href="#">Brand</a></li>
-      <li><a href="#">Apps</a></li>
-      <li><a href="#">Advertise</a></li>
-      <li><a href="#">Businesses</a></li>
-      <li><a href="#">Developers</a></li>
-      <li><a href="#">Directory</a></li>
-      <li><a href="#">Settings</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/help">Help Center</a></li>
+      <li><a href="/blog">Blog</a></li>
+      <li><a href="//status.twitter.com">Status</a></li>
+      <li><a href="/careers">Jobs</a></li>
+      <li><a href="/terms">Terms</a></li>
+      <li><a href="/privacy">Privacy Policy</a></li>
+      <li><a href="/privacy#cookies">Cookies</a></li>
+      <li><a href="/ads">Ads Info</a></li>
+      <li><a href="/brands">Brand</a></li>
+      <li><a href="/apps">Apps</a></li>
+      <li><a href="/ads/signup">Advertise</a></li>
+      <li><a href="/business">Businesses</a></li>
+      <li><a href="//develop.twitter.com">Developers</a></li>
+      <li><a href="/dir">Directory</a></li>
+      <li><a href="/settings">Settings</a></li>
       <li>&copy; 2017 Twitter</li>
     </ul>
   </div>

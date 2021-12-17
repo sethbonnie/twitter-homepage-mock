83f5662d49cbd3698765d447af8594d2bbb434b0
Seth Bonnie <sethkbonnie@gmail.com>
2021-12-17 10:37:00 -0600

    Fix images and create custom random int/bool generators

--- src/FeaturedTweets/index.js
+++ src/FeaturedTweets/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import faker from 'faker';
-import { drop, range, take } from 'lodash';
+import { drop, range, take, sample } from 'lodash';
 import Heading from './heading';
 import Column from './column';
 import './featured-tweets.css';
 
+const randomInt = (max) => Math.round(Math.random() * max);
+
+const randomBool = () => Boolean(randomInt(1));
+
 const genName = () => 
-  faker.random.number(1) ?
+  randomInt(1) ?
     faker.company.companyName() :
     `${faker.name.firstName()} ${faker.name.lastName()}`
+
+const avatars = [
+  'https://parade.com/wp-content/uploads/2021/08/albert-einstein-quotes.jpg',
+  'https://pbs.twimg.com/profile_images/879355674957926400/VSGZHGib_400x400.jpg',
+  'https://www.thefamouspeople.com/profiles/images/henri-poincare-3.jpg',
+  'https://i1.wp.com/www.cherrychapman.com/wp-content/uploads/2019/11/th-1.jpeg?fit=338%2C396&ssl=1',
+  'https://m.media-amazon.com/images/M/MV5BZmU5OGE0ZmUtZjdmYi00NDE2LWI2NzYtMzAwYzJiY2IwMDJlXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg',
+  'https://cdn.britannica.com/w:400,h:300,c:crop/78/23678-050-1DCF79A2/Auguste-Comte-Tony-Toullion-Bibliotheque-Nationale-Paris.jpg',
+  'https://www.azquotes.com/public/pictures/authors/5b/83/5b83eea35cf5550675a976ecae99a87c/5475aadeae20e_sofia_kovalevskaya.jpg',
+];
+
+const images = [
+  'https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/247851/pexels-photo-247851.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/1066176/pexels-photo-1066176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+];
 
 class FeaturedTweets extends Component {
   constructor(props) {
@@ -16,16 +37,16 @@ class FeaturedTweets extends Component {
     this.state = {
       tweets: range(60).map((id) => ({
         id,
-        image: faker.image.image(),
+        image: sample(images),
         name: genName(),
         category: faker.random.word(),
-        verified: faker.random.boolean(),
+        verified: randomBool(),
         username: faker.internet.userName(),
         posted: faker.date.recent(),
-        retweets: faker.random.number(1000),
-        favorites: faker.random.number(1000),
+        retweets: randomInt(1000),
+        favorites: randomInt(1000),
         content: faker.lorem.sentence(),
-        userAvatar: faker.image.avatar(),
+        userAvatar: sample(avatars),
       }))
     }
   }
--- src/Moments/index.js
+++ src/Moments/index.js
@@ -5,14 +5,22 @@ import Moment from './moment';
 import SignupPrompt from './signup-prompt';
 import './moments.css';
 
+const images = [
+  'https://images.pexels.com/photos/341013/pexels-photo-341013.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/1701193/pexels-photo-1701193.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/801625/pexels-photo-801625.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/2101187/pexels-photo-2101187.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+];
+
 class Moments extends Component {
   constructor(props) {
     super(props);
     this.state = {
       moments: range(3).map(i => ({
+        id: i,
         title: faker.hacker.phrase(),
         category: faker.random.word(),
-        image: faker.image.image(),
+        image: images[i],
         posted: faker.date.recent()
       }))
     }
@@ -25,7 +33,7 @@ class Moments extends Component {
           Moments <span>·</span> <a href="/moments">View All</a>
         </h4>
         <div id="moments">
-          { moments.map(moment => <Moment {...moment} />)}
+          { moments.map(moment => <Moment key={moment.id} {...moment} />)}
           <SignupPrompt />
         </div>
       </div>
